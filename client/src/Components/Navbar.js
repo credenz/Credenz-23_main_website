12a9d8b014ed62eb16b6456ed7e565c2bdbf9d09
VanshTeppalwar <vanshteppalwar@gmail.com>
2023-03-08 17:50:07 +0530

    removed scss file in profile page

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import { debounce } from "debounce";
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './Profile.scss'
+import './Profile.css'
 const Profile = () => {
     const [data,setData]=useState({});
     const buttons = document.querySelectorAll(".card-buttons button");
@@ -104,16 +104,30 @@ const Profile = () => {
                                 
                                 <div class="card-contact">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16"> <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/> </svg>
-                                    <p>{data.referral}<br></br><a href={ data.referral} onClick={(e)=>{e.preventDefault();navigator.clipboard.writeText(data.referral);alert("Copied the text: " + data.referral);}}> Copy Code To Clipboard</a></p>
+                                    <p>{data.referral}<br></br><a href={ data.referral} onClick={(e)=>{e.preventDefault();navigator.clipboard
+                                                                                                                                    .writeText(`${data.referral}`)
+                                                                                                                                    .then(() => {
+                                                                                                                                        alert("successfully copied");
+                                                                                                                                    })
+                                                                                                                                    .catch(() => {
+                                                                                                                                        alert("something went wrong");
+                                                                                                                                    });}}> Copy Code To Clipboard</a></p>
                                 </div>
                                 <div class="card-contact">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16"> <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/> </svg>
-                                    <p><a href={`https://localhost:3000/register/${ data.referral}`} target="_blank"> {data.referral}</a><br></br><a href={`https://localhost:3000/register/${ data.referral}`} onClick={(e)=>{e.preventDefault();navigator.clipboard.writeText(e.target.href);alert("Copied the text: " + e.target.href);}}> Copy Link To Clipboard</a></p>
+                                    <p><a href={`https://localhost:3000/register/${ data.referral}`} target="_blank" rel="noopener noreferrer"> {data.referral}</a><br></br><a href={`https://localhost:3000/register/${ data.referral}`} onClick={(e)=>{e.preventDefault();navigator.clipboard
+                                                                                                                                    .writeText(`${e.target.href}`)
+                                                                                                                                    .then(() => {
+                                                                                                                                        alert("successfully copied");
+                                                                                                                                    })
+                                                                                                                                    .catch(() => {
+                                                                                                                                        alert("something went wrong");
+                                                                                                                                    });}}> Copy Link To Clipboard</a></p>
                                 </div>
                                     <div class="card-contact">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-qr-code-scan" viewBox="0 0 16 16"> <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0v-3Zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5ZM.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5Zm15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5ZM4 4h1v1H4V4Z"/> <path d="M7 2H2v5h5V2ZM3 3h3v3H3V3Zm2 8H4v1h1v-1Z"/> <path d="M7 9H2v5h5V9Zm-4 1h3v3H3v-3Zm8-6h1v1h-1V4Z"/> <path d="M9 2h5v5H9V2Zm1 1v3h3V3h-3ZM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8H8Zm2 2H9V9h1v1Zm4 2h-1v1h-2v1h3v-2Zm-4 2v-1H8v1h2Z"/> <path d="M12 9h2V8h-2v1Z"/> </svg>
                                     <img src={`https://chart.googleapis.com/chart?cht=qr&chl=https://localhost:3000/register/${ data.referral}&chs=100x100&chld=L|0`}
-                                                class="qr-code img-thumbnail img-responsive" />
+                                                class="qr-code img-thumbnail img-responsive" alt='QR CODE'/>
                                     </div>
                                 <div class="card-contact">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/> </svg>
