37ba81b156c8769b62572660f9faa2e66e1b5474
Yash <yashsharadmali@gmail.com>
2023-03-30 01:55:57 +0530

    bgchange

--- client/src/pages/Events/EventSky.js
+++ client/src/pages/Events/EventSky.js
@@ -1,66 +1,66 @@
-// import React from 'react'
-// import Particles from "react-particles";
-// import { loadTrianglesPreset } from "tsparticles-preset-triangles";
+// // import React from 'react'
+// // import Particles from "react-particles";
+// // import { loadTrianglesPreset } from "tsparticles-preset-triangles";
 
-// export default class ParticlesContainer extends React.PureComponent{
-//   // this customizes the component tsParticles installation
-//   async customInit(engine){
-//     // this adds the preset to tsParticles, you can safely use the
-//     await loadTrianglesPreset(engine);
-//   }
+// // export default class ParticlesContainer extends React.PureComponent{
+// //   // this customizes the component tsParticles installation
+// //   async customInit(engine){
+// //     // this adds the preset to tsParticles, you can safely use the
+// //     await loadTrianglesPreset(engine);
+// //   }
 
-//   render() {
-//     const options = {
-//       preset: "triangles",
-//     };
+// //   render() {
+// //     const options = {
+// //       preset: "triangles",
+// //     };
 
-//     return <Particles options={options} init={this.customInit} />;
-//   }
+// //     return <Particles options={options} init={this.customInit} />;
+// //   }
+// // }
+
+
+// // import React from "react";
+// // import WAVES from "vanta/dist/vanta.waves.min";
+// // Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
+
+
+// import React, { useState, useEffect, useRef } from 'react'
+// import CLOUDS from 'vanta/dist/vanta.clouds.min'
+// // Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
+
+//  const EventSky = ({children}) => {
+//   const [vantaEffect, setVantaEffect] = useState(0)
+//   const myRef = useRef(null)
+//   useEffect(() => {
+//     if (!vantaEffect) {
+//       setVantaEffect(CLOUDS({
+//         el: myRef.current,
+//         THREE: window.THREE,
+//         mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   backgroundColor: 0x161414,
+//   skyColor: 0x2a0216,
+//   cloudColor: 0x591037,
+//   cloudShadowColor: 0x134493,
+//   sunColor: 0x0,
+//   sunGlareColor: 0xfcb6ce,
+//   sunlightColor: 0x0,
+//   speed: 1.60
+//   //       color1: 0x1beded,
+//   // color2: 0x6060c
+//       }))
+//     }
+//     return () => {
+//       if (vantaEffect) vantaEffect.destroy()
+//     }
+//   }, [vantaEffect])
+//   return <div ref={myRef}>
+//     {children}
+//   </div>
 // }
 
-
-// import React from "react";
-// import WAVES from "vanta/dist/vanta.waves.min";
-// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
-
-
-import React, { useState, useEffect, useRef } from 'react'
-import CLOUDS from 'vanta/dist/vanta.clouds.min'
-// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
-
- const EventSky = ({children}) => {
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const myRef = useRef(null)
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(CLOUDS({
-        el: myRef.current,
-        THREE: window.THREE,
-        mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  backgroundColor: 0x161414,
-  skyColor: 0x2a0216,
-  cloudColor: 0x591037,
-  cloudShadowColor: 0x134493,
-  sunColor: 0x0,
-  sunGlareColor: 0xfcb6ce,
-  sunlightColor: 0x0,
-  speed: 1.60
-  //       color1: 0x1beded,
-  // color2: 0x6060c
-      }))
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
-  return <div ref={myRef}>
-    {children}
-  </div>
-}
-
-export default EventSky;
+// export default EventSky;
 
--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -282,11 +282,11 @@ const handelSubmit = (eventId) =>{
 
   return (
     <>
-      <EventSky>
+      {/* <EventSky> */}
       <div className="st">
-        {/* <div id="stars"></div>
+        <div id="stars"></div>
         <div id="stars2"></div>
-        <div id="stars3"></div> */}
+        <div id="stars3"></div>
         <div className="eventpage " data-aos="fade-in"  data-aos-duration="400" >
           <div className="container " >
             <h1 style={{ textAlign: "center" }}>EVENTS</h1>
@@ -319,7 +319,7 @@ const handelSubmit = (eventId) =>{
           </div>
         </div>
       </div>
-        </EventSky>
+        {/* </EventSky> */}
 
       <Modal 
         isOpen={modalIsOpen}
