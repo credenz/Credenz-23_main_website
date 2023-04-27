d9d6aff7442a919a1f26629550ae08ee490a7bb0
Devraj Shetake <devrajshetake@gmail.com>
2023-04-28 01:03:06 +0530

    new logo

--- client/src/Components/Home/Experience.js
+++ client/src/Components/Home/Experience.js
@@ -15,6 +15,8 @@ import { LogoV7 } from './models/Logo_v7'
 import {isMobile} from 'react-device-detect';
 import Sign from './models/Sign'
 import Soon from './models/Soon'
+import JustLogo from './models/just_logo'
+import JustLogoV2 from './models/just_logo_v2'
 
 export default function Experience({explore3D, isMouseDown}) {
   const [isSnapped, setSnpped] = useState(false)
@@ -137,6 +139,8 @@ export default function Experience({explore3D, isMouseDown}) {
         
 
         <Soon/>
+        {/* <JustLogo /> */}
+        <JustLogoV2 />
 
 
         
